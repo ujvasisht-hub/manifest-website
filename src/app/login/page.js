5f2f955d3d893353f106c2bea9e29b94ebfe5289
Ujjwal Vasisht <ujjwalvasisht@Ujjwals-MacBook-Pro.local>
2025-08-23 16:07:33 +0530

    Fix supabaseClient import paths

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { supabase } from '../utils/supabaseClient';
+import { supabase } from '../../utils/supabaseClient';
 import { useRouter } from 'next/navigation';
 
 const LoginPage = () => {
